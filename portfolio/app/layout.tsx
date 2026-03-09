6fb0e2f5cbdd6db58ea88d0be93cad8890dafcf5
Gracol-Composites <153564370+Gracol-Composites@users.noreply.github.com>
2026-03-09 15:11:49 +1300

    adding analytics and insights

--- portfolio/app/layout.tsx
+++ portfolio/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -42,6 +44,8 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} ${inter.variable} antialiased`}
       >
         {children}
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
